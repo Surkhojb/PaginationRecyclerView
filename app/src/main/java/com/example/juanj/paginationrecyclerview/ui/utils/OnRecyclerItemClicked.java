31f4b2adcb8f66eb11b58bdbc30ab6519a0c113e
Surkhojb <juanjo@ragecoders.com>
2018-03-28 12:55:42 +0200

    Item click recyclerview implemented

---
+++ app/src/main/java/com/example/juanj/paginationrecyclerview/ui/utils/OnRecyclerItemClicked.java
@@ -0,0 +1,11 @@
+package com.example.juanj.paginationrecyclerview.ui.utils;
+
+import android.view.View;
+
+/**
+ * Created by juanj on 28/03/2018.
+ */
+
+public interface OnRecyclerItemClicked {
+    void onItemClicked(View v, int position);
+}
