cce74a8ef1fc59befe0ca9ebd2424c5f123272d5
Surkhojb <juanjo@ragecoders.com>
2018-03-28 12:55:48 +0200

    Item click recyclerview implemented

--- app/src/main/java/com/example/juanj/paginationrecyclerview/ui/main/MainActivity.java
+++ app/src/main/java/com/example/juanj/paginationrecyclerview/ui/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.juanj.paginationrecyclerview.ui.main;
 
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -15,7 +16,9 @@ import com.example.juanj.paginationrecyclerview.MyApplication;
 import com.example.juanj.paginationrecyclerview.R;
 import com.example.juanj.paginationrecyclerview.di.DaggerMainComponent;
 import com.example.juanj.paginationrecyclerview.di.modules.MainModule;
+import com.example.juanj.paginationrecyclerview.ui.detail.DetailActivity;
 import com.example.juanj.paginationrecyclerview.ui.utils.OnLoadMoreListener;
+import com.example.juanj.paginationrecyclerview.ui.utils.OnRecyclerItemClicked;
 
 import javax.inject.Inject;
 
@@ -23,7 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements OnRecyclerItemClicked{
     private boolean isLoadingMore = false;
 
     @BindView(R.id.pb_loading_more)
@@ -76,6 +79,14 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+    }
+
+    @Override
+    public void onItemClicked(View v, int position) {
+        Intent intentToDetails = new Intent(this, DetailActivity.class);
+        intentToDetails.putExtra(DetailActivity.EXTRA_VIDEO,rvAdapter.getVideo(position));
+
+        startActivity(intentToDetails);
     }
 
     private void injectDependencies() {
